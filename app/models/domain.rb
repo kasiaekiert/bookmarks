bbc40d082fd9e9d7a23808d3ae165c1a1e1daeb6
Kasia <kasia.ekiert@gmail.com>
2020-05-06 10:29:01 +0200

    assign domain to url

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,5 @@
 class Domain < ApplicationRecord
   has_many :marks
 
-  validates :link, url: true
 
 end
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,3 +1,14 @@
 class Mark < ApplicationRecord
-  belongs_to :domain
+  require 'uri'
+
+  belongs_to :domain, required: false
+
+  validates :url, url: true, presence: true
+
+  after_create :assign_domain
+
+  def assign_domain
+    domain_name = URI.parse(url.to_s).host
+    domain = Domain.find_or_create_by(name: domain_name)
+  end
 end
